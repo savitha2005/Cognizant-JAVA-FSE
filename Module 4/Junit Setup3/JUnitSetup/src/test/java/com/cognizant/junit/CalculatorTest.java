8150e5afed617f1a9e9d45f9ca106673b13b448c
savitha2005 <727823tucs307@skct.edu.in>
2026-06-30 14:22:34 +0530

    mod 4 3

---
+++ Module 4/Junit Setup3/JUnitSetup/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.cognizant.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup completed");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown completed");
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
